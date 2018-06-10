d0b36cdbbddc5b6bc21e2c6944574388ffbde76c
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-10 15:38:22 -0300

    Added copy task

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -5,8 +5,15 @@ var imagemin = require('gulp-imagemin');
 
 //Tarefas
 
+//Cópia
+gulp.task('copy', function(){
+    gulp.src('src/img/**/*')
+        .pipe(gulp.dest('dist'));
+});
+
+//Otimização de imagens
 gulp.task('build-img', function(){
     gulp.src('dist/img/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
 });

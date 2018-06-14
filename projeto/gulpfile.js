80a505f9fe5bd93b1a5664b9eabc0fc98eee80a4
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-14 10:43:49 -0300

    Using gulp-concat plugin and creating task to minify js files

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 
 //Tarefas
 gulp.task('copy', ['clean'], function(){
@@ -19,4 +20,10 @@ gulp.task('build-img', ['copy'], function(){
     return gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build-js', function(){
+    gulp.src('dist/js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'));
 });

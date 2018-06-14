07144129b41a8b243d96f736b93761c25d49e61f
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-14 11:38:06 -0300

    Using cssmin plugin and fixing copy function

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -6,39 +6,36 @@ var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var htmlReplace = require('gulp-html-replace');
 var uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
+var cssmin = require('gulp-cssmin');
 
 //Tarefas
-gulp.task('default', ['copy'], function(){
-    gulp.start('build-img', 'build-html', 'build-js');
-})
+gulp.task('default', ['copy'], function() {
+    gulp.start('build-img', 'usemin');
+});
 
-gulp.task('copy', ['clean'], function(){
-    gulp.src('src/**/*')
+gulp.task('copy', ['clean'], function() {
+    return gulp.src('src/**/*')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function(){
+gulp.task('clean', function() {
     return gulp.src('dist')
         .pipe(clean());
 });
 
-gulp.task('build-img', function(){
-    return gulp.src('dist/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'));
+gulp.task('build-img', function() {
+
+  return gulp.src('dist/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('build-js', function(){
-    gulp.src('dist/js/**/*.js')
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('build-html', function(){
-    gulp.src('dist/**/*.html')
-        .pipe(htmlReplace({
-            'js': 'js/all.js'
-        }))
-        .pipe(gulp.dest('dist/'));
+gulp.task('usemin', function() {
+  return gulp.src('dist/**/*.html')
+    .pipe(usemin({
+      js: [uglify],
+      css: [cssmin]
+    }))
+    .pipe(gulp.dest('dist'));
 });
